db7badca07e675b83f04c2a90ae111ea0f0864f4
Stéphane Dion <divers@sdion.net>
2013-10-13 07:49:37 +0200

    -

--- scripts/content.js
+++ scripts/content.js
@@ -9,7 +9,10 @@ chrome.storage.local.get(null, function(cfg) {
 		options = cfg;
 
 		if(options.url) {
-			if(url.indexOf(options.url) != -1) {
+			if(options.url.slice(-1) === '/') {
+				options.url = options.url.slice(0, -1);
+			}
+			if(url.indexOf(options.url + '/home') != -1) {
 				setInterval('refresh()', 5000);
 			}
 		}
